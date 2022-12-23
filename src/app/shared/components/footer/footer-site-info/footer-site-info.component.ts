66147c495e3ad8f8ec1e81479f986f36ca9d14ad
Xoje Laptop <dawxoje@gmail.com>
2022-12-23 19:12:30 +0100

    all footer styled but issue with backtotop icon

--- src/app/shared/components/footer/footer-site-info/footer-site-info.component.ts
+++ src/app/shared/components/footer/footer-site-info/footer-site-info.component.ts
@@ -3,14 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-footer-site-info',
   template: `
-      <div class="back-to-top__wrapper">
-          <a mat-flat-button class="back-to-top" href="#page" title="Volver arriba">
-              <mat-icon aria-label="Side nav toggle icon">
-                  keyboard_double_arrow_up
-              </mat-icon>
-          </a>
-      </div>
       <div class="footer__site-info">
+          <div class="back-to-top__wrapper">
+              <a mat-flat-button class="back-to-top" href="#page" title="Volver arriba">
+                  <mat-icon aria-label="Side nav toggle icon" class="back-to-top__icon">
+                      keyboard_double_arrow_up
+                  </mat-icon>
+              </a>
+          </div>
           <p>
               <a href="https://puertasch.com/aviso-legal/">Aviso Legal</a>
               |
@@ -42,33 +42,55 @@ import { Component } from '@angular/core';
   styles: [
     `
       .footer__site-info {
-        background-color: grey;
+        background-color: #515151;
         color: white;
+
         p {
           text-align: center;
         }
+
+        a {
+          color: white;
+
+          &:hover {
+            text-decoration: underline;
+          }
+        }
       }
+
       .back-to-top__wrapper {
-        background: #222222;
+
+
+        .back-to-top {
+          background-color: #515151;
+
+          border-radius: 0px 20px 0 0;
+          color: #999999;
+          display: block;
+          font-size: 30px;
+          height: 50px;
+          line-height: 50px;
+          margin-left: auto;
+          //position: absolute;
+          right: 60px;
+          top: -15px;
+          transition: all 0.2s linear 0s;
+          width: 50px;
+          z-index: 99;
+          transform: rotate(-45deg);
+
+          &__icon {
+            visibility: visible;
+            animation-duration: 2s;
+            animation-name: flash;
+            transform: rotate(45deg);
+            left: 30px;
+            top: 5px;
+          }
+        }
+
       }
-      .back-to-top {
-        border-radius: 0px 20px 0 0;
-        color: #999999;
-        background: #222222;
-        display: block;
-        font-size: 30px;
-        height: 50px;
-        line-height: 50px;
-        margin: auto;
-        //position: absolute;
-        right: 60px;
-        top: -25px;
-        transition: all 0.2s linear 0s;
-        width: 50px;
-        z-index: 99;
-        transform: rotate(-45deg);
-      }
-    `,
+	`,
   ],
 })
 export class FooterSiteInfoComponent {}
