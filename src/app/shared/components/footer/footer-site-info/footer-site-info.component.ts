156dcddcc1bc40a77fea705b76d788e6e9ae5d10
Xoje <dawxoje@gmail.com>
2022-12-25 17:40:36 +0100

    Fix: Working with whatsapp-fixed.component.ts , now in process

--- src/app/shared/components/footer/footer-site-info/footer-site-info.component.ts
+++ src/app/shared/components/footer/footer-site-info/footer-site-info.component.ts
@@ -1,96 +1,106 @@
 import { Component } from '@angular/core';
+import { faAngleDoubleUp } from '@fortawesome/free-solid-svg-icons/faAngleDoubleUp';
 
 @Component({
-  selector: 'app-footer-site-info',
-  template: `
-      <div class="footer__site-info">
-          <div class="back-to-top__wrapper">
-              <a mat-flat-button class="back-to-top" href="#page" title="Volver arriba">
-                  <mat-icon aria-label="Side nav toggle icon" class="back-to-top__icon">
-                      keyboard_double_arrow_up
-                  </mat-icon>
-              </a>
-          </div>
-          <p>
-              <a href="https://puertasch.com/aviso-legal/">Aviso Legal</a>
-              |
-              <a href="https://puertasch.com/politica-de-privacidad/">Política de Privacidad</a>
-              |
-              <a href="https://puertasch.com/politica-de-cookies/">Política de cookies</a>
-              2021 Todos los derechos reservados.
-          </p>
-          <p>
-              Desarrollado por
-              <a href="">Luque&Vilches</a>
-          </p>
-          <p>
-              <!-- <a href="https://intelligentlife.es"
-                          ><img
-                              class="alignnone"
-                              style="width: 50px"
-                              title="Intelligent Life"
-                              src="https://puertasch.com/wp-content/uploads/2021/12/TamgrandeIntelligentLife.png"
-                              alt=""
-                              width="900"
-                              height="900"
-                      /></a> -->
+    selector: 'app-footer-site-info',
+    template: `
+        <div class="footer__site-info">
+            <div class="back-to-top__wrapper">
+                <a
+                    class="back-to-top"
+                    href="#page"
+                    mat-flat-button
+                    title="Volver arriba">
+                    <!--<mat-icon
+                        aria-label="Side nav toggle icon"
+                        class="back-to-top__icon">
+                        keyboard_double_arrow_up
+                    </mat-icon>-->
+                    <fa-icon
+                        [icon]="arrowIcon"
+                        class="back-to-top__icon"></fa-icon>
+                </a>
+            </div>
+            <p>
+                <a href="https://puertasch.com/aviso-legal/">Aviso Legal</a>
+                |
+                <a href="https://puertasch.com/politica-de-privacidad/"
+                    >Política de Privacidad</a
+                >
+                |
+                <a href="https://puertasch.com/politica-de-cookies/"
+                    >Política de cookies</a
+                >
+                2021 Todos los derechos reservados.
+            </p>
+            <p>
+                Desarrollado por
+                <a href="">Luque&Vilches</a>
+            </p>
+            <p>
+                <!-- <a href="https://intelligentlife.es"
+          ><img
+              class="alignnone"
+              style="width: 50px"
+              title="Intelligent Life"
+              src="https://puertasch.com/wp-content/uploads/2021/12/TamgrandeIntelligentLife.png"
+              alt=""
+              width="900"
+              height="900"
+      /></a> -->
 
-              Copyright © 2022 Puertas CH
-          </p>
-      </div>
-  `,
-  styles: [
-    `
-      .footer__site-info {
-        background-color: #515151;
-        color: white;
+                Copyright © 2022 Puertas CH
+            </p>
+        </div>
+    `,
+    styles: [
+        `
+            .footer__site-info {
+                background-color: #515151;
+                color: white;
 
-        p {
-          text-align: center;
-        }
+                p {
+                    text-align: center;
+                }
 
-        a {
-          color: white;
+                a {
+                    color: white;
 
-          &:hover {
-            text-decoration: underline;
-          }
-        }
-      }
+                    &:hover {
+                        text-decoration: underline;
+                    }
+                }
+            }
 
-      .back-to-top__wrapper {
+            .back-to-top__wrapper {
+                .back-to-top {
+                    border: 3px solid orange;
+                    background-color: #515151;
+                    border-top-right-radius: 20px;
+                    color: #999999;
+                    display: block;
+                    height: 50px;
+                    width: 50px;
 
+                    line-height: 50px;
+                    margin-left: auto;
+                    right: 60px;
+                    top: -15px;
 
-        .back-to-top {
-          background-color: #515151;
+                    transition: all 0.2s linear 0s;
+                    transform: rotate(-45deg);
 
-          border-radius: 0px 20px 0 0;
-          color: #999999;
-          display: block;
-          font-size: 30px;
-          height: 50px;
-          line-height: 50px;
-          margin-left: auto;
-          //position: absolute;
-          right: 60px;
-          top: -15px;
-          transition: all 0.2s linear 0s;
-          width: 50px;
-          z-index: 99;
-          transform: rotate(-45deg);
-
-          &__icon {
-            visibility: visible;
-            animation-duration: 2s;
-            animation-name: flash;
-            transform: rotate(45deg);
-            left: 30px;
-            top: 5px;
-          }
-        }
-
-      }
-	`,
-  ],
+                    &__icon {
+                        border: 3px solid red;
+                        animation-duration: 2s;
+                        animation-name: flash;
+                        transform: rotate(45deg);
+                    }
+                }
+            }
+        `,
+    ],
 })
-export class FooterSiteInfoComponent {}
+export class FooterSiteInfoComponent {
+    arrowIcon = faAngleDoubleUp;
+}
--- src/app/shared/components/whatsapp-fixed/whatsapp-fixed.component.ts
+++ src/app/shared/components/whatsapp-fixed/whatsapp-fixed.component.ts
@@ -1,34 +1,57 @@
 import { Component } from '@angular/core';
 import { faWhatsappSquare } from '@fortawesome/free-brands-svg-icons';
+import { SizeProp } from '@fortawesome/fontawesome-svg-core';
 
 @Component({
-  selector: 'app-whatsapp-fixed',
-  template: `
-    <button
-      mat-raised-button
-      matTooltip="Escribenos y te atendemos ahora"
-      matTooltipPosition="right"
-      aria-label="Button that displays a tooltip when focused or hovered over">
-      <fa-icon
-        [style.background-color]="'#25D366'"
-        [icon]="whatsapp_icon"
-        size="3x"></fa-icon>
-    </button>
-  `,
-  styles: [
-    `
-      fa-icon {
-        color: rgb(37, 211, 102);
-      }
-
-      button {
-        position: fixed;
-        right: 0;
-        bottom: 50px;
-      }
+    selector: 'app-whatsapp-fixed',
+    template: `
+        <button
+            aria-label="Button that displays a tooltip when focused or hovered over"
+            mat-icon-button
+            matTooltip="Escribenos y te atendemos ahora"
+            matTooltipPosition="right">
+            <fa-icon
+                [icon]="whatsapp_icon.icon"
+                [size]="whatsapp_icon.size"
+                [fixedWidth]="whatsapp_icon.fixedWidth"
+                [border]="true"
+                pull="right"
+                class="what_icon"></fa-icon>
+        </button>
     `,
-  ],
+    styles: [
+        `
+            button {
+                position: fixed;
+                right: 0;
+                bottom: 50px;
+                fa-icon.what_icon ::ng-deep svg path {
+                    fill: rgb(37, 211, 102);
+                    background-color: white;
+                }
+                fa-icon.what_icon {
+                    background-color: blue;
+                    //color: rgb(37, 211, 102);
+
+                    svg {
+                        background-color: white;
+
+                        path {
+                            fill: #ffffff;
+                            stroke: #ff0000;
+                            stroke-width: 10;
+                        }
+                    }
+                }
+            }
+        `,
+    ],
 })
 export class WhatsappFixedComponent {
-  whatsapp_icon = faWhatsappSquare;
+    whatsapp_icon = {
+        icon: faWhatsappSquare,
+        size: '3x' as SizeProp,
+        fixedWidth: true,
+        pull: 'right',
+    };
 }
