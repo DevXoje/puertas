8d8a93f39b64477173e6381425c334bced705f70
Xoje Laptop <dawxoje@gmail.com>
2022-12-25 20:23:32 +0100

    create div for the checkbox input

--- src/app/pages/home/home/home.component.ts
+++ src/app/pages/home/home/home.component.ts
@@ -1,16 +1,16 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  template: `
-    <app-hero-parallax></app-hero-parallax>
-    <app-who-iam></app-who-iam>
-    <app-reviews></app-reviews>
-    <app-call-to-action></app-call-to-action>
-  `,
-  styles: [
-    `
-      /* app-hero-parallax {
+    selector: 'app-home',
+    template: `
+        <app-hero-parallax></app-hero-parallax>
+        <app-who-iam></app-who-iam>
+        <app-reviews></app-reviews>
+        <app-call-to-action></app-call-to-action>
+    `,
+    styles: [
+        `
+            /* app-hero-parallax {
         position: absolute;
         width: 95vw;
         height: 100px;
@@ -20,7 +20,7 @@ import { Component } from '@angular/core';
       app-who-iam {
         margin-top: 100px;
       }*/
-    `,
-  ],
+        `,
+    ],
 })
 export class HomeComponent {}
--- src/app/shared/components/contact-form/contact-form.component.ts
+++ src/app/shared/components/contact-form/contact-form.component.ts
@@ -1,57 +1,57 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
-import { Input } from '../../../core/models/input';
+import { Input } from '@core/models/input';
 
 @Component({
-  selector: 'app-contact-form',
-  templateUrl: './contact-form.component.html',
-  styleUrls: ['./contact-form.component.scss'],
+    selector: 'app-contact-form',
+    templateUrl: './contact-form.component.html',
+    styleUrls: ['./contact-form.component.scss'],
 })
 export class ContactFormComponent {
-  addressForm = this.fb.group({
-    name: [null, Validators.required],
-    email: [null, Validators.required],
-    telephone: [null, Validators.required],
-    subject: [null, Validators.required],
-    message: null,
-    acceptance_279: [null, Validators.required],
-    acceptance_414: [null, Validators.required],
-    acceptance_415: [null, Validators.required],
-  });
+    addressForm = this.fb.group({
+        name: [null, Validators.required],
+        email: [null, Validators.required],
+        telephone: [null, Validators.required],
+        subject: [null, Validators.required],
+        message: null,
+        acceptance_279: [null, Validators.required],
+        acceptance_414: [null, Validators.required],
+        acceptance_415: [null, Validators.required],
+    });
 
-  inputs: Input[] = [
-    {
-      label: 'Tu nombre',
-      type: 'text',
-      name: 'name',
-      placeholder: 'algo',
-    },
-    {
-      label: 'Tu correo electrónico',
-      type: 'email',
-      name: 'email',
-      placeholder: 'algo',
-    },
-    {
-      label: 'Tu teléfono',
-      type: 'tel',
-      name: 'telephone',
-      placeholder: 'algo',
-    },
-    {
-      label: 'Asunto',
-      type: 'text',
-      name: 'subject',
-      placeholder: 'algo',
-    },
-    {
-      label: 'Tu mensaje (opcional)',
-      type: 'textarea',
-      name: 'message',
-      placeholder: 'algo',
-    },
-    {
-      label: `He leído y acepto el
+    inputs: Input[] = [
+        {
+            label: 'Tu nombre',
+            type: 'text',
+            name: 'name',
+            placeholder: 'algo',
+        },
+        {
+            label: 'Tu correo electrónico',
+            type: 'email',
+            name: 'email',
+            placeholder: 'algo',
+        },
+        {
+            label: 'Tu teléfono',
+            type: 'tel',
+            name: 'telephone',
+            placeholder: 'algo',
+        },
+        {
+            label: 'Asunto',
+            type: 'text',
+            name: 'subject',
+            placeholder: 'algo',
+        },
+        {
+            label: 'Tu mensaje (opcional)',
+            type: 'textarea',
+            name: 'message',
+            placeholder: 'algo',
+        },
+        {
+            label: `He leído y acepto el
 			   <a
 				   target="_blank"
 				   rel="nofollow noopener noreferrer"
@@ -65,29 +65,27 @@ export class ContactFormComponent {
 				   href="https://puertasch.com/politica-de-privacidad"
 				   >Política de Privacidad</a
 			   >.`,
-      type: 'checkbox',
-      name: 'acceptance_279',
-      placeholder: 'algo',
-    },
-    {
-      label:
-        'Declaro, bajo mi propia responsabilidad, ser mayor de 18 años y respondo de manera exclusiva de la veracidad de dicha declaración.',
-      type: 'checkbox',
-      name: 'acceptance_414',
-      placeholder: 'algo',
-    },
-    {
-      label:
-        'Acepto recibir la información que la entidad considere oportuno enviarme por correo electrónico o medio de comunicación electrónica equivalente. (Es posible darse de baja en cualquier momento).',
-      type: 'checkbox',
-      name: 'acceptance_415',
-      placeholder: 'algo',
-    },
-  ];
+            type: 'checkbox',
+            name: 'acceptance_279',
+            placeholder: 'algo',
+        },
+        {
+            label: 'Declaro, bajo mi propia responsabilidad, ser mayor de 18 años y respondo de manera exclusiva de la veracidad de dicha declaración.',
+            type: 'checkbox',
+            name: 'acceptance_414',
+            placeholder: 'algo',
+        },
+        {
+            label: 'Acepto recibir la información que la entidad considere oportuno enviarme por correo electrónico o medio de comunicación electrónica equivalente. (Es posible darse de baja en cualquier momento).',
+            type: 'checkbox',
+            name: 'acceptance_415',
+            placeholder: 'algo',
+        },
+    ];
 
-  constructor(private fb: FormBuilder) {}
+    constructor(private fb: FormBuilder) {}
 
-  onSubmit(): void {
-    alert('Thanks!');
-  }
+    onSubmit(): void {
+        alert('Thanks!');
+    }
 }
